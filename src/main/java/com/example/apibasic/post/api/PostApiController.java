5cefebecd6afe6a57bf16a99f2c6876fb179560c
KYUNGMINNA <nkm4095@naver.com>
2023-01-16 09:44:01 +0900

    # post api service layer 의존성 주입

--- src/main/java/com/example/apibasic/post/api/PostApiController.java
+++ src/main/java/com/example/apibasic/post/api/PostApiController.java
@@ -4,6 +4,7 @@ package com.example.apibasic.post.api;
 import com.example.apibasic.post.dto.*;
 import com.example.apibasic.post.entity.PostEntity;
 import com.example.apibasic.post.repository.PostRepository;
+import com.example.apibasic.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,7 +39,8 @@ public class PostApiController {
     //private PostRepository postRepository=new PostRepository();
 
 
-    private final PostRepository postRepository;
+    //PostService에 의존 관계
+    private final PostService postService;
 
 
     //@Autowired : 스프링 컨테이너에게 의존 객체를 자동 주입 해달라
@@ -55,25 +57,21 @@ public class PostApiController {
     @GetMapping   //RequestMapping이 있어 비워놔도 된다.
     public ResponseEntity<?> list(){
         log.info("/posts GET request");
-        List<PostEntity> list=postRepository.findAll();
-
-        //엔터티 리스트를 DTO리스트로 변환해서 클라이언트에 응답
-        List<PostResponseDTO> responseDTOList=list.stream()
-                //.map(et->new PostResponseDTO(et))
-                .map(PostResponseDTO::new)
-                .collect(toList());
-
-        PostListResponseDTO listResponseDTO = PostListResponseDTO.builder()
-                .count(responseDTOList.size())
-                .posts(responseDTOList)
-                .build();
 
 
+        try {
+            //Service에서 isEMpty() 있으면 throw new 하는 코드 있어
+            //예외처리해야함
+            PostListResponseDTO listResponseDTO = postService.getList();
 
-
-        return  ResponseEntity
-                .ok()
-                .body(listResponseDTO);
+            return  ResponseEntity
+                    .ok()
+                    .body(listResponseDTO);
+        } catch (Exception e) { //실무에서는 Catch가 다양하게 있을 수 있다.
+             return ResponseEntity
+                    .notFound()
+                    .build();
+        }
     }
 
     //게시물 개별 조회시에는 클라이언트에게  추가로 수정시간 정보를 제공하세요
@@ -85,14 +83,17 @@ public class PostApiController {
             @PathVariable Long postNo){
         log.info("/posts/{} GET request",postNo);
 
-        PostEntity post = postRepository.findOne(postNo);
+        try {
+            PostDetailResponseDTO dto = postService.getDetail(postNo);
 
-        //엔티티를 DTO로 변환
-        PostResponseDTO postResponseDTO=new PostDetailResponseDTO(post);
-
-        return ResponseEntity
-                .ok()
-                .body(postResponseDTO);
+            return ResponseEntity
+                    .ok()
+                    .body(dto);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .notFound()
+                    .build();
+        }
     }
 
 
@@ -100,51 +101,43 @@ public class PostApiController {
     @PostMapping
     public ResponseEntity<?> create(@RequestBody PostCreateDTO createDTO) {
         log.info("/posts POST request");
-        log.info("게시물 정보 : {}",createDTO);
+        log.info("게시물 정보: {}", createDTO);
 
-        //dto를 entity로 변환 작업
-        PostEntity entity=createDTO.toEntity();
-
-        boolean flag=postRepository.save(entity);
-
-        return flag?ResponseEntity.ok().body("INSERT-SUCCESS")
-                :ResponseEntity.badRequest().body("INSERT_FAIL");
+        return postService.insert(createDTO)
+                ? ResponseEntity.ok().body("INSERT-SUCCESS")
+                : ResponseEntity.badRequest().body("INSERT-FAIL")
+                ;
     }
+
 
     //게시물 수정은 제목 ,내용만 수정 가능해야 합니다.
     // 게시물 수정
     @PatchMapping("/{postNo}")
-    public ResponseEntity<?> modify(@PathVariable Long postNo,@RequestBody PostModifyDTO modifyDTO) {
+    public ResponseEntity<?> modify(
+            @PathVariable Long postNo
+            , @RequestBody PostModifyDTO modifyDTO
+    ) {
         log.info("/posts/{} PATCH request", postNo);
+        log.info("수정할 정보 : {}", modifyDTO);
 
-        // 수정 전 데이터 조회하기
-        PostEntity entity = postRepository.findOne(postNo);
-        // 수정 진행
-        String modTitle = modifyDTO.getTitle();
-        String modContent = modifyDTO.getContent();
-
-        if (modTitle != null) entity.setTitle(modTitle);
-        if (modContent != null) entity.setContent(modContent);
-        entity.setModifyDate(LocalDateTime.now());
-
-        boolean flag = postRepository.save(entity);
-        return flag
+        return postService.update(postNo, modifyDTO)
                 ? ResponseEntity.ok().body("MODIFY-SUCCESS")
                 : ResponseEntity.badRequest().body("MODIFY-FAIL")
                 ;
-
     }
+
 
 
     // 게시물 삭제
     @DeleteMapping("/{postNo}")
     public ResponseEntity<?> remove(@PathVariable Long postNo) {
         log.info("/posts/{} DELETE request", postNo);
-        boolean flag = postRepository.delete(postNo);
-        return flag
+
+        return postService.delete(postNo)
                 ? ResponseEntity.ok().body("DELETE-SUCCESS")
                 : ResponseEntity.badRequest().body("DELETE-FAIL")
                 ;
     }
+
 
 }
